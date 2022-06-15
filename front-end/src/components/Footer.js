d1e6f5f2f2a58e3c56a7a3c43c9214643c7d31b7
RizkiB12 <rbagus306@gmail.com>
2022-06-15 17:00:16 +0700

    add footer

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const footer = () => {
+ return(
+    <div>
+        <hr />
+        <p>2022 Copyright Store. All Rights Reserved.</p>
+    </div>
+ )
+
+}
+
+export default footer;
--- front-end/src/components/NavigationBar.js
+++ front-end/src/components/NavigationBar.js
@@ -1,42 +1,30 @@
 import React from "react";
 
 const navBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarNav">
-                    <ul className="navbar-nav">
-                    <a className="navbar-brand" href="google.com">Navbar</a>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="google.com">Home</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle active" href="google.com" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="google.com">Action</a></li>
-                                <li><a className="dropdown-item" href="google.com">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="google.com">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="google.com">Rewards</a>
-                        </li>
-                    </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                <button className="btn btn-success">Sign In</button>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg bg-light mx-5">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"><img src="" alt="" />Logo</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-3" type="search" placeholder="Search" aria-label="Search" />
+          </form>
+          <button className="btn btn-outline-success" type="submit">Sign In</button>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default navBar;
