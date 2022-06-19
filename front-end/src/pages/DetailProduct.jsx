c03c88e4bc596ba9250b600aa4a62460a3c7f57f
RizkiB12 <rbagus306@gmail.com>
2022-06-19 19:24:48 +0700

    add detail product in pages

---
+++ front-end/src/pages/DetailProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DetailProduct() {
+    return (
+        <div>
+            <h1>
+                Detail Product
+            </h1>
+        </div>
+    )
+}
