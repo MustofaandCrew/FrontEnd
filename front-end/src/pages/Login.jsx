3b5e1b049e654d7611d461289662b0625ffceac2
garincsr <garincaesar69@gmail.com>
2022-06-21 19:10:51 +0700

    responsive login register

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -10,11 +10,11 @@ export default function Login() {
                     <img src={Pic} alt="woman" className="img-fluid" />
                 </div>
                 <div className="col-6">
-                    <h3>
+                    <h3 className="login-title">
                         Belanja kebutuhan utama,<br />
                         menjadi lebih mudah
                     </h3>
-                    <form>
+                    <form className="login-form">
                         <div className="mb-3">
                             <label for="exampleInputEmail1" className="form-label">Email Address</label>
                             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
@@ -23,8 +23,10 @@ export default function Login() {
                             <label for="exampleInputPassword1" className="form-label">Password</label>
                             <input type="password" className="form-control" id="exampleInputPassword1" />
                         </div>
+                        <div className="login-btn">
                         <button type="submit" className="btn btn-success me-3">Sign In</button>
                         <a href="/register" className="btn btn-outline-secondary">Sign Up</a>
+                        </div>
                     </form>
                 </div>
             </div>
