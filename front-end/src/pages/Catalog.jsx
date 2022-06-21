0a968816c962325d6ddf470971edb8c33f5854c4
garincsr <garincaesar69@gmail.com>
2022-06-21 21:31:22 +0700

    responsive catalog

--- front-end/src/pages/Catalog.jsx
+++ front-end/src/pages/Catalog.jsx
@@ -10,7 +10,9 @@ import NewProducts3 from "../assets/images/NewProducts3.jpg";
 export default function Catalog() {
     return (
         <div class="container">
-            <div class="row">
+            
+            {/* for desktop or tablet layout */}
+            <div class="row catalog-horizontal">
                 <div class="col-9">
                     <h2 className="catalog-title">Catalog</h2>
                 </div>
@@ -64,6 +66,56 @@ export default function Catalog() {
                     </div>
                 </div>
             </div>
+
+            {/* for Phone Layout */}
+            <div className="catalog-vertical row row-cols-1 row-cols-md-4 g-1 mt-4">
+                <div className="catalog-nav">
+                    <h5 className="catalog-nav-title">Categories</h5>
+                    <br />
+                    <div className="catalog-nav-link d-flex justify-content-between">
+                        <a href="/" className="catalog-link">All Categories</a>
+                        <i className="bx bx-chevron-right"></i>
+                    </div>
+                    <hr />
+                    <div className="catalog-nav-link d-flex justify-content-between">
+                        <a href="/" className="catalog-link">Wishlist</a>
+                        <i className="bx bx-chevron-right"></i>
+                    </div>
+                    <hr />
+                </div>
+
+                <div className="row row-cols-2 row-cols-md-4 g-4 mt-2">
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                    <Card image={NewProducts2} />
+                    <Card image={NewProducts3} />
+                    <Card image={NewProducts} />
+                </div>
+            </div>
+
         </div>
     )
 }
